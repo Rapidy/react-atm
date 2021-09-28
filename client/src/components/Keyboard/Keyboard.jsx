2adf653f7770f49bcbd08486de2494e15d6d4707
Rapidy <artemchik03211@gmail.com>
2021-09-28 19:17:01 +0500

    button rendering, strings to numbers

--- client/src/components/Keyboard/Keyboard.jsx
+++ client/src/components/Keyboard/Keyboard.jsx
@@ -11,10 +11,10 @@ import './keyboard.sass';
 export default function Keyboard() {
   const dispatch = useDispatch();
 
-  const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-  function addNumber(e) {
-    dispatch(inputNumber(e.target.innerText));
+  function addNumber(item) {
+    dispatch(inputNumber(item));
   }
 
   function clearNumbers() {
@@ -45,55 +45,32 @@ export default function Keyboard() {
   return (
     <section className='keyboard'>
       <div className='keyboard-digits'>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          1
-        </button>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          2
-        </button>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          3
-        </button>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          4
-        </button>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          5
-        </button>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          6
-        </button>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          7
-        </button>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          8
-        </button>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          9
-        </button>
-        <button
-          onClick={addNumber}
-          className='keyboard__btn keyboard-digits__btn'
-          disabled>
-          –
-        </button>
-        <button onClick={addNumber} className='keyboard__btn keyboard-digits__btn'>
-          0
-        </button>
-        <button className='keyboard__btn keyboard-digits__btn' disabled>
-          +
-        </button>
+        {digits.map((item, i) => {
+          return (
+            <button
+              key={i}
+              onClick={() => addNumber(item)}
+              className='keyboard__btn keyboard-digits__btn'>
+              {item}
+            </button>
+          );
+        })}
       </div>
 
       <div className='keyboard-functions'>
-        <button onClick={clearNumbers} className='keyboard__btn keyboard-functions__btn'>
+        <button
+          onClick={clearNumbers}
+          className='keyboard__btn keyboard-functions__btn keyboard-functions__btn--clear'>
           <span></span>Очистить
         </button>
-        <button onClick={deleteNumber} className='keyboard__btn keyboard-functions__btn'>
+        <button
+          onClick={deleteNumber}
+          className='keyboard__btn keyboard-functions__btn keyboard-functions__btn--erase'>
           <span></span>Стереть
         </button>
-        <button onClick={withdrawMoney} className='keyboard__btn keyboard-functions__btn'>
+        <button
+          onClick={withdrawMoney}
+          className='keyboard__btn keyboard-functions__btn keyboard-functions__btn--withdraw'>
           <span></span>Выдача
         </button>
       </div>
